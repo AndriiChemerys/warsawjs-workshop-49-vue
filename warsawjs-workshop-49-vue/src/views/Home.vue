cf2aa93038b8cc59c222df65e1974d93ac143a30
AndriiChemerys <andriichemerys@gmail.com>
2020-10-18 13:25:08 +0200

    Creating router posts for each article

--- warsawjs-workshop-49-vue/src/views/Home.vue
+++ warsawjs-workshop-49-vue/src/views/Home.vue
@@ -7,6 +7,7 @@
     <div v-for="post in posts">
       <h2>{{post.title}} by {{post.author}}.</h2>
       <p>{{post.text}}</p>
+      <router-link :to="post.link">Czytaj dalej</router-link>
       <p>{{post.data}}</p>
     </div>
 
@@ -21,17 +22,19 @@ export default {
 
   created(){
     fetch('https://sheets.googleapis.com/v4/spreadsheets/1qHcnrzbNZ1XHs85IfPeZWoLpZuwWp5nWGadjFFRpE3s/values/vue?key=AIzaSyAtgGjAz3Vk3wFJmoRPYuuRCEwRZpeFy0g')
+    // fetch(`https://sheets.googleapis.com/v4/spreadsheets/1qHcnrzbNZ1XHs85IfPeZWoLpZuwWp5nWGadjFFRpE3s/values/vue!A${ postId }:D${ postId }?key=AIzaSyAtgGjAz3Vk3wFJmoRPYuuRCEwRZpeFy0g`)
     .then(res=>res.json())
     .then(res=>{
       let {values} = res;
       console.log(res)
       let posts = [];
-      values.map(v=>{
+      values.map((v,i)=>{
         posts.push({
           data: v[0],
           title: v[1],
           author: v[2],
-          text: v[3]
+          text: v[3].substring(0, 150)+'...',
+          link: `/post/${i+1}`
         })
       })
       this.posts=posts;
@@ -42,32 +45,32 @@ export default {
     return {
       title: 'WarsawJS',
 
-      posts: [],
+      posts: null,
 
-      person: {
-        data: {
-          name: 'Janek'
-        }
-      },
+    //   person: {
+    //     data: {
+    //       name: 'Janek'
+    //     }
+    //   },
 
-      articles: [{
-        data: '12-12-2020',
-        title: 'The Success Principles',
-        author: 'Jack Canfield',
-        text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour'
-      },
-      {
-        data: '11-11-2020',
-        title: 'The Only Skill That Matters',
-        author: 'Jonathan A.',
-        text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using'
-      },
-      {
-        data: '10-10-2020',
-        title: 'Think and Grow Rich',
-        author: 'Napoleon Hill ',
-        text: 'The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using'
-      },]
+    //   articles: [{
+    //     data: '12-12-2020',
+    //     title: 'The Success Principles',
+    //     author: 'Jack Canfield',
+    //     text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour'
+    //   },
+    //   {
+    //     data: '11-11-2020',
+    //     title: 'The Only Skill That Matters',
+    //     author: 'Jonathan A.',
+    //     text: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using'
+    //   },
+    //   {
+    //     data: '10-10-2020',
+    //     title: 'Think and Grow Rich',
+    //     author: 'Napoleon Hill ',
+    //     text: 'The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using'
+    //   },]
 
     }
   }
--- warsawjs-workshop-49-vue/src/views/Post.vue
+++ warsawjs-workshop-49-vue/src/views/Post.vue
@@ -1,12 +1,46 @@
 <template>
     <div>
-        Post
+        <div v-for="post in posts">
+            <h2>{{post.title}} by {{post.author}}.</h2>
+            <p>{{post.text}}</p>
+            <p>{{post.data}}</p>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    created(){
+        console.log(this)
+        const {postId} = this.$route.params
+        console.log(postId)
+        // fetch('https://sheets.googleapis.com/v4/spreadsheets/1qHcnrzbNZ1XHs85IfPeZWoLpZuwWp5nWGadjFFRpE3s/values/vue?key=AIzaSyAtgGjAz3Vk3wFJmoRPYuuRCEwRZpeFy0g')
+        fetch(`https://sheets.googleapis.com/v4/spreadsheets/1qHcnrzbNZ1XHs85IfPeZWoLpZuwWp5nWGadjFFRpE3s/values/vue!A${ postId }:D${ postId }?key=AIzaSyAtgGjAz3Vk3wFJmoRPYuuRCEwRZpeFy0g`)
+        .then(res=>res.json())
+        .then(res=>{
+        let {values} = res;
+        console.log(res)
+        let posts = [];
+        values.map((v,i)=>{
+            posts.push({
+            data: v[0],
+            title: v[1],
+            author: v[2],
+            text: v[3],
+            link: `/post/${i+1}`
+            })
+        })
+        this.posts=posts;
+        console.log(this.posts)
+        }).catch(err=>console.error(err))
+  },
+  data(){
+    return {
+      title: 'WarsawJS',
 
+      posts: null,
+    }
+  }
 }
 </script>
 
